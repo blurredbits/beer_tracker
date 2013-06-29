ef18cb149d7b77be8480dd0ea7648977092b0ab8
Blurredbits <mmorris00@mac.com>
2013-06-29 12:18:27 -0500

    Static pages structure done

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,12 +6,22 @@ describe "StaticPages" do
       visit '/static_pages/home'
       page.should have_content('Beer Tracker')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', text: "Beer Tracker | Home")
+    end
   end
 
   describe "About Page" do
     it "should have the content About" do
       visit '/static_pages/about'
       page.should have_content('About')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title', text: "Beer Tracker | About")
     end
   end
 
